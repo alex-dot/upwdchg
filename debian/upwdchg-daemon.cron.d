f9b748437c3497d773705f68854358f6897ff3bc
Cédric Dufour <cedric.dufour@ced-network.net>
2018-10-15 15:56:51 +0200

    * NEW MAJOR RELEASE (3.0.20181015a) - WARNING: INCOMPATIBLE WITH VERSION 2.x!

--- debian/upwdchg-daemon.cron.d
+++ debian/upwdchg-daemon.cron.d
@@ -1,4 +1,5 @@
-## UPwdChg contab
+## UPwdChg crontab
 
 # Clean-up stale password nonces
-0 0 * * *  root  test -x /usr/bin/upwdchg-daemon && eval "$(/usr/bin/upwdchg-daemon --showconf 'token.public_directory')" && [ -n "${token_public_directory}" ] && find "${token_public_directory}" -type f -name '*.nonce' -mmin +1440 -delete
+0 0 * * *  root  test -x /usr/bin/upwdchg-daemon && eval "$(/usr/bin/upwdchg-daemon --showconf 'frontend.tokens_directory')" && [ -n "${frontend_tokens_directory}" ] && find "${frontend_tokens_directory}" -type f -name '*.nonce' -mmin +1440 -delete
+
